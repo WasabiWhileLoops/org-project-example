d51d85029bdfb0a18a3655d4428fe70697f94cf8
Philippe <philippe.sa.costa@gmail.com>
2026-05-26 16:01:13 +0200

    feat(exercise3): add logging to tests

--- tests/e2e/test_e2e_example.py
+++ tests/e2e/test_e2e_example.py
@@ -1,5 +1,10 @@
+import logging
 import requests
 
+logger = logging.getLogger(__name__)
+
 def test_external_ping():
-    resp = requests.get("https://httpbin.org/get", timeout=5)
+    url = "https://httpbin.org/get"
+    logger.info("Requesting resource from %s", url)
+    resp = requests.get(url, timeout=5)
     assert resp.status_code == 200
--- tests/unit/test_unit_example.py
+++ tests/unit/test_unit_example.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 def test_math_addition():
-    # Very simple unit test just to prove pytest works
+    logger.info("Performing dummy test")
     assert 1 + 1 == 2
