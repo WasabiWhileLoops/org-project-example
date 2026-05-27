b22a5db34a5e36eaaa500e2aa6b7518f08d1a88f
Philippe Costa <32560769+philippesamuel@users.noreply.github.com>
2026-05-27 12:40:53 +0200

    feat(exercise3): save test logs in workflow (#2)
    Update tests and workflow in exercise 3:
    - allow test failures and update test assertion
    - update GitHub Actions workflow and add act configuration
    - add `.actrc` configuration file for `act` tool to simulate GitHub Actions
      locally with ARM64 support and artifact server settings
    - add custom Dockerfile for `act` to include Python 3.11 and `uv` for local
      testing
    - downgrade `actions/upload-artifact` from v6 to v5 in `003-save-test-logs`
      workflow for compatibility

--- tests/unit/test_unit_example.py
+++ tests/unit/test_unit_example.py
@@ -5,3 +5,10 @@ logger = logging.getLogger(__name__)
 def test_math_addition():
     logger.info("Performing dummy test")
     assert 1 + 1 == 2
+    
+
+# should fail. testing ci workflow
+def test_fail_math_addition():
+    logger.info("Performing dummy test that should fail")
+    assert 1 + 1 == 3
+    
